1020f482ad0ea84846c7452f0874f7d02be384e9
FLYS <woaihuameifeihua@163.com>
2018-09-23 13:59:58 -0400

    create enum for cards

---
+++ maven-A1/src/test/java/FileReadingTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileReadingTest {
+
+	@Test
+	public void test() {
+		App test = new App();
+		assertEquals(1,1);
+	}
+
+}
